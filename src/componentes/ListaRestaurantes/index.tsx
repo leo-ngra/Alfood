2ce49435707e7e045ad0939f78d106a1bd80bd14
leo-ngra <leonardo.ngra@gmail.com>
2022-12-16 09:54:54 -0300

    Put e Delete no formulário

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -1,9 +1,14 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import { useEffect, useState } from 'react';
 import { IPaginacao } from '../../interfaces/IPaginacao';
 import IRestaurante from '../../interfaces/IRestaurante';
 import style from './ListaRestaurantes.module.scss';
 import Restaurante from './Restaurante';
+
+interface IParametrosBusca {
+  ordering?: string
+  search?: string
+}
 
 const ListaRestaurantes = () => {
 
@@ -11,8 +16,11 @@ const ListaRestaurantes = () => {
   const [proximaPagina, setProximaPagina] = useState('')
   const [paginaAnterior, setPaginaAnterior] = useState('')
 
-  const carregarDados = (url: string) => {
-    axios.get<IPaginacao<IRestaurante>>(url)
+  const [busca, setBusca] = useState('')
+
+  const carregarDados = (url: string, opcoes: AxiosRequestConfig = {}) => {
+
+    axios.get<IPaginacao<IRestaurante>>(url, opcoes)
       .then(resposta => {
         setRestaurantes(resposta.data.results)
         setProximaPagina(resposta.data.next)
@@ -23,12 +31,29 @@ const ListaRestaurantes = () => {
       })
   }
 
+  const buscar = (evento: React.FormEvent<HTMLFormElement>) => {
+    evento.preventDefault()
+    const opcoes = {
+      params: {
+
+      } as IParametrosBusca
+    }
+    if (busca) {
+      opcoes.params.search = busca
+    }
+    carregarDados('http://localhost:8000/api/v1/restaurantes/', opcoes)
+  }
+
   useEffect(() => {
     carregarDados('http://localhost:8000/api/v1/restaurantes/')
   }, [])
 
   return (<section className={style.ListaRestaurantes}>
     <h1>Os restaurantes mais <em>bacanas</em>!</h1>
+    <form onSubmit={buscar}>
+      <input type="text" value={busca} onChange={evento => setBusca(evento.target.value)} />
+      <button type='submit'>buscar</button>
+    </form>
     {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
     {<button onClick={() => carregarDados(paginaAnterior)} disabled={!paginaAnterior}>
       Página Anterior
--- src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
+++ src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
@@ -1,4 +1,4 @@
-import { Paper, TableContainer } from "@mui/material"
+import { Button, Paper, TableContainer } from "@mui/material"
 import Table from "@mui/material/Table"
 import TableBody from "@mui/material/TableBody"
 import TableCell from "@mui/material/TableCell"
@@ -6,6 +6,7 @@ import TableHead from "@mui/material/TableHead"
 import TableRow from "@mui/material/TableRow"
 import axios from "axios"
 import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
 
 import IRestaurante from "../../../interfaces/IRestaurante"
 
@@ -20,12 +21,22 @@ const AdministracaoRestaurantes = () => {
 
     }, [])
 
+    const excluir = (restauranteASerExcluido: IRestaurante) => {
+        axios.delete(`http://localhost:8000/api/v2/restaurantes/${restauranteASerExcluido.id}/`)
+            .then(() => {
+                const listaRestaurante = restaurantes.filter(restaurante => restaurante.id !== restauranteASerExcluido.id)
+                setRestaurantes([...listaRestaurante])
+            })
+    }
+ 
     return (
         <TableContainer component={Paper}>
             <Table>
                 <TableHead>
                     <TableRow>
                         <TableCell>Nome</TableCell>
+                        <TableCell>Editar</TableCell>
+                        <TableCell>Excluir</TableCell>
                     </TableRow>
                 </TableHead>
 
@@ -33,6 +44,12 @@ const AdministracaoRestaurantes = () => {
                     {restaurantes.map(restaurante => (
                         <TableRow key={restaurante.id}>
                             <TableCell>{restaurante.nome}</TableCell>
+                            <TableCell>
+                                [ <Link to={`/admin/restaurantes/${restaurante.id}`}>Editar</Link>]
+                            </TableCell>
+                            <TableCell>
+                                <Button variant='outlined' color='error' onClick={() => excluir(restaurante)}>Excluir</Button>
+                            </TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
